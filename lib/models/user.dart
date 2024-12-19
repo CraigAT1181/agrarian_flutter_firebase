5f2c9e4df000693a2984a470ae4e8d3e261bdbe5
craig <craigtipple81@gmail.com>
2024-12-19 17:10:39 +0000

    Added initial files for flutter to android build.gradle and root gradle files.

---
+++ lib/models/user.dart
@@ -0,0 +1,21 @@
+class User {
+  final String userId;
+  final String authId;
+  final String userName;
+  final String email;
+  final String profilePic;
+  final String town;
+  final String allotment;
+  final String plot;
+
+  User({
+    required this.userId,
+    required this.authId,
+    required this.userName,
+    required this.email,
+    required this.profilePic,
+    required this.town,
+    required this.allotment,
+    required this.plot,
+  });
+}
